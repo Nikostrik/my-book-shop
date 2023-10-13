faaef19088950afab8867c809a1f32ab09ced820
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-10-13 13:11:35 +0300

    fixed mistakes

--- src/main/java/project/mybookshop/controller/BookController.java
+++ src/main/java/project/mybookshop/controller/BookController.java
@@ -2,18 +2,18 @@ package project.mybookshop.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import project.mybookshop.dto.BookDto;
 import project.mybookshop.dto.CreateBookRequestDto;
 import project.mybookshop.service.BookService;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping(value = "/api/books")
 public class BookController {
     private final BookService bookService;
