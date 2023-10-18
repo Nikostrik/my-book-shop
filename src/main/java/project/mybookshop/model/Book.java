4b48286ffdac8fdb305797c106d2ebb34816e797
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-10-18 10:27:15 +0300

    added liquibase support

--- src/main/java/project/mybookshop/model/Book.java
+++ src/main/java/project/mybookshop/model/Book.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Entity
+@Table(name = "books")
 @Data
 public class Book {
     @Id
@@ -26,6 +28,7 @@ public class Book {
 
     private String description;
 
+    @Column(name = "cover_image")
     private String coverImage;
 
     @Column(nullable = false)
