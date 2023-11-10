982d7d406d0d7aefbc0925a56e965e982d0a386f
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-11-10 21:57:03 +0200

    added fixes after review

--- src/main/java/project/mybookshop/model/Role.java
+++ src/main/java/project/mybookshop/model/Role.java
@@ -9,9 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE roles SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "roles")
 public class Role {
     @Id
@@ -21,6 +25,9 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
     private RoleName name;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
 
     public enum RoleName {
         ADMIN,
