bdae1b1ea65fcc58dc13c7df2a8c087634974f5d
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-12-07 16:41:09 +0200

    covered in docker

--- src/test/java/project/mybookshop/MyBookShopApplicationTests.java
+++ src/test/java/project/mybookshop/MyBookShopApplicationTests.java
@@ -1,13 +1,10 @@
 package project.mybookshop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyBookShopApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
